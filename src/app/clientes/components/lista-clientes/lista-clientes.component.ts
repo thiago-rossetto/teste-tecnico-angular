dc86fca808c14b0fd9468e9d05e80e24fe3ba175
Thiago Bessa Rossetto <thiago.rossetto@gpnet.com.br>
2023-04-10 11:58:06 -0300

    :sparkles: feat: Filtro/Pesquisa com nome, cpf ou data nascimento

--- src/app/clientes/components/lista-clientes/lista-clientes.component.ts
+++ src/app/clientes/components/lista-clientes/lista-clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FilterPipe } from 'ngx-filter-pipe';
 
 import { Cliente } from '../../models/cliente.model';
 import { ClienteService } from '../../services/cliente.service';
@@ -13,7 +14,10 @@ export class ListaClientesComponent implements OnInit {
 
   @ViewChild('closebutton') closebutton: any;
   clientes: Array<Cliente> = [];
+  clientesFiltrados: Array<Cliente> = [];
   idExclusao: number | null = null;
+  query: any = "";
+  tipoFiltro: string | null = null;
 
   constructor(
     private router: Router,
@@ -25,8 +29,43 @@ export class ListaClientesComponent implements OnInit {
     this.service.listarClientes().subscribe(
       (res) => {
         this.clientes = res;
+        this.clientesFiltrados = res;
       }
     )
+  }
+
+  aplicarTipoPesquisa(tipo: any) {
+    this.tipoFiltro = tipo.target.value;
+    this.query = "";
+    this.ngOnInit();
+  }
+
+  filtroPesquisa() { 
+    if (this.tipoFiltro == "nome") {
+      this.clientes = this.clientesFiltrados.filter(
+        (a) => a.nome == this.query
+      );
+    }
+    else if(this.tipoFiltro == "cpf") {
+      this.query = this.query.match(/\d/g)!.join("");
+      this.clientes = this.clientesFiltrados.filter(
+        (a) => a.cpf == this.query
+      );
+    }
+    else if(this.tipoFiltro == "data_nasc") {
+      const query = this.formatarDataPesquisa(this.query);
+      this.clientes = this.clientesFiltrados.filter(
+        (a) => a.data_nasc == query
+      );
+    }
+  }
+
+  formatarDataPesquisa(data: string) {
+    var dia  = data.split("/")[0];
+    var mes  = data.split("/")[1];
+    var ano  = data.split("/")[2];
+  
+    return ano + '-' + ("0"+mes).slice(-2) + '-' + ("0"+dia).slice(-2);
   }
 
   adicionarCliente() {
